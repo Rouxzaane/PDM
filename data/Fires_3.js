39a99d5fd781a3bbea6465466e7618738ca3c7ab
Rouxzaane <vrnrou001@myuct.ac.za>
2025-05-23 14:11:48 +0200

    Add files via upload

---
+++ data/Fires_3.js
@@ -0,0 +1,14 @@
+var json_Fires_3 = {
+"type": "FeatureCollection",
+"name": "Fires_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.455666387975356, -34.10746577401909 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.439108741323434, -34.112738502285083 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.430878905117748, -34.091970163227607 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.389925672570396, -34.054233387043823 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.420395661379555, -34.18846724806275 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.404523834411446, -34.070709789093357 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Fires": "Fires" }, "geometry": { "type": "Point", "coordinates": [ 18.419024022011943, -33.951163017371115 ] } }
+]
+}
---
+++ data/Floods_4.js
@@ -0,0 +1,14 @@
+var json_Floods_4 = {
+"type": "FeatureCollection",
+"name": "Floods_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.337901350841609, -34.146557263387749 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.453902851645591, -34.110872805239197 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.370036901740011, -33.988458226884923 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.351617744517757, -33.992357434763171 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.431956621763756, -34.093998545915973 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.33515807210637, -34.023869462241187 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Floods": "Floods" }, "geometry": { "type": "Point", "coordinates": [ 18.374445742564465, -33.809389855122177 ] } }
+]
+}
